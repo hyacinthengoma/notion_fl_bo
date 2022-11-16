825d7b61be309db76c2d16728655bc70a5e3e080
Hyacinthe N'Goma <hyacinthengoma@gmail.com>
2022-11-16 13:30:37 +0100

    debug video in process

--- notion-blog/src/pages/le-cabinet.tsx
+++ notion-blog/src/pages/le-cabinet.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     return (
         <>
             <Header></Header>
+            <video autoPlay loop muted playsInline className='absolute w-full h-full object-cover'>
+                <source src="./notion-blog/public/pexels-pavel-danilyuk-8061612.mp4" type='video/mp4; codecs=vp9'/>
+            </video>
             <div className={sharedStyles.layout}>
                 <h1>Accueil</h1>
             </div>
