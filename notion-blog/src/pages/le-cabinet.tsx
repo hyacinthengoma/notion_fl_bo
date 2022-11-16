f26e9b3d955e2c06329e8b4075f006944e876bce
Hyacinthe N'Goma <hyacinthengoma@gmail.com>
2022-11-16 13:41:17 +0100

    debug video ok

--- notion-blog/src/pages/le-cabinet.tsx
+++ notion-blog/src/pages/le-cabinet.tsx
@@ -5,9 +5,15 @@ export default function Home() {
     return (
         <>
             <Header></Header>
-            <video autoPlay loop muted playsInline className='absolute w-full h-full object-cover'>
+
+            <video autoPlay loop muted playsInline className='absolute w-full h-full object-cover' style={{ width: "400px", height:"400px" }}>
                 <source src="./notion-blog/public/pexels-pavel-danilyuk-8061612.mp4" type='video/mp4; codecs=vp9'/>
             </video>
+
+
+            <video autoPlay loop muted playsInline src={"/pexels-pavel-danilyuk-8061612.mp4"} style={{ width: "100%", height:"100vh" }} />
+
+
             <div className={sharedStyles.layout}>
                 <h1>Accueil</h1>
             </div>
