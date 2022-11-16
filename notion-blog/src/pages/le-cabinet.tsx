62a9bc22cbad9d7c2befd415b3cbb8df15bb2b4e
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2022-11-16 16:02:14 +0100

    pages expertise

--- notion-blog/src/pages/le-cabinet.tsx
+++ notion-blog/src/pages/le-cabinet.tsx
@@ -6,11 +6,11 @@ export default function Home() {
         <>
             <Header></Header>
 
-<div>
-<video autoPlay loop muted playsInline className='absolute w-full h-full object-cover' src={"/pexels-pavel-danilyuk-8061612.mp4"} style={{ width: "100%", height:"50%" }}/>
-<h1>MAÎTRE FLORENCE BABEAU </h1>
+            <div>
+            <video autoPlay loop muted playsInline className='absolute w-full h-full object-cover' src={"/pexels-pavel-danilyuk-8061612.mp4"} style={{ width: "100%", height:"50%" }}/>
+            <h1>MAÎTRE FLORENCE BABEAU </h1>
 
-</div>
+            </div>
 
             <div className={sharedStyles.layout}>
                 <h1>Accueil</h1>
