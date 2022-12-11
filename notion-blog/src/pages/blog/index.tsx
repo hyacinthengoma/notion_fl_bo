b05d0d44e7fe42ccfae58ee1cf49fdd8f1932a30
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2022-12-11 17:09:13 +0100

    commit blog part 1/?
    page de blog commencé

--- notion-blog/src/pages/blog/index.tsx
+++ notion-blog/src/pages/blog/index.tsx
@@ -50,53 +50,42 @@ export async function getStaticProps({ preview }) {
 const Index = ({ posts = [], preview }) => {
   return (
     <>
-      <Header titlePre="Blog" />
-      {preview && (
-        <div className={blogStyles.previewAlertContainer}>
-          <div className={blogStyles.previewAlert}>
-            <b>Note:</b>
-            {` `}Viewing in preview mode{' '}
-            <Link href={`/api/clear-preview`}>
-              <button className={blogStyles.escapePreview}>Exit Preview</button>
-            </Link>
-          </div>
-        </div>
-      )}
-      <div className={`${sharedStyles.layout} ${blogStyles.blogIndex}`}>
-        <h1>My Notion Blog</h1>
-        {posts.length === 0 && (
-          <p className={blogStyles.noPosts}>There are no posts yet</p>
-        )}
-        {posts.map((post) => {
-          return (
-            <div className={blogStyles.postPreview} key={post.Slug}>
-              <h3>
-                <span className={blogStyles.titleContainer}>
-                  {!post.Published && (
-                    <span className={blogStyles.draftBadge}>Draft</span>
-                  )}
-                  <Link href="/blog/[slug]" as={getBlogLink(post.Slug)}>
-                    {post.Page}
-                  </Link>
-                </span>
-              </h3>
-              {post.Authors.length > 0 && (
-                <div className="authors">By: {post.Authors.join(' ')}</div>
-              )}
-              {post.Date && (
-                <div className="posted">Posted: {getDateStr(post.Date)}</div>
-              )}
-              <p>
-                {(!post.preview || post.preview.length === 0) &&
-                  'No preview available'}
-                {(post.preview || []).map((block, idx) =>
-                  textBlock(block, true, `${post.Slug}${idx}`)
-                )}
-              </p>
+        <Header/>
+        <div className={`${sharedStyles.layout} ${blogStyles.blogIndex}`}>
+            <div>
+                <h1>TEST</h1>
             </div>
-          )
-        })}
-      </div>
+            <div>
+                <h2 className={"text-center text-black font-bold text-2xl my-5"}>DERNIERES ACTUALITES</h2>
+                <div className={"grid grid-cols-1 md:grid-cols-4 gap-4 content-center"}>
+                    {posts.map((post) => {
+                        return(
+                                <div className="card card-compact w-96 bg-gray-100 shadow-xl rounded-none mx-auto" key={post.Slug}>
+                                    <Link href="/blog/[slug]" as={getBlogLink(post.Slug)}>
+                                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-black">{post.Page}</h2>
+                                        <p>
+                                            {(!post.preview || post.preview.length === 0) &&
+                                                'Pas de résumé disponible'}
+                                            {(post.preview || []).map((block, idx) =>
+                                                textBlock(block, true, `${post.Slug}${idx}`)
+                                            )}
+                                        </p>
+                                        <p>
+                                            {post.Date && (
+                                                <p className="posted">Publié le : {getDateStr(post.Date)}</p>
+                                            )}
+                                        </p>
+                                    </div>
+                                    </Link>
+                                </div>
+
+                        )
+                    })};
+                </div>
+            </div>
+        </div>
     </>
   )
 }
