af138ba15a40090a1c80e61f0363cea4be7e9f69
Hyacinthe N'Goma <hyacinthengoma@gmail.com>
2023-01-21 16:25:49 +0100

    change accordion

--- notion-blog/src/pages/Expertise/Droit-travail/index.tsx
+++ notion-blog/src/pages/Expertise/Droit-travail/index.tsx
@@ -9,13 +9,126 @@ export default function HomeDroitTravail() {
     return (
         <>
             <Header></Header>
+
+
             <div className={"relative w-full"}>
                 <div className={"absolute transform top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-4/5 text-center z-50"}>
                     <h1 className={"text-white text-2xl md:text-4xl font-bold"}>DROIT DU TRAVAIL</h1>
                 </div>
-                <img className={"brightness-50 h-96 w-full object-cover"} src={"/images/FlorenceBabeau/banniere.png"} alt={"image-banniere"}></img>
+                <img className={"brightness-50 h-[25vh] w-full object-cover"} src={"/images/FlorenceBabeau/banniere.png"} alt={"image-banniere"}></img>
             </div>
-            <div className={"w-full flex bg-white"}>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <link rel="stylesheet" href="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/flowbite.min.css"/>
+
+
+
+            <div className="max-w-2xl mx-auto">
+
+                <div className="border-b border-gray-200 dark:border-gray-700 mb-4">
+                    <ul className="flex flex-wrap -mb-px" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+                        <li className="mr-2" role="presentation">
+                            <button
+                                className="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                                id="profile-tab" data-tabs-target="#profile" type="button" role="tab"
+                                aria-controls="profile" aria-selected="false">Contrat de travail et relations individuelles
+                            </button>
+                        </li>
+                        <li className="mr-2" role="presentation">
+                            <button
+                                className="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300 active"
+                                id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab"
+                                aria-controls="dashboard" aria-selected="true">Dashboard
+                            </button>
+                        </li>
+                        <li className="mr-2" role="presentation">
+                            <button
+                                className="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                                id="settings-tab" data-tabs-target="#settings" type="button" role="tab"
+                                aria-controls="settings" aria-selected="false">Settings
+                            </button>
+                        </li>
+                        <li role="presentation">
+                            <button
+                                className="inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                                id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab"
+                                aria-controls="contacts" aria-selected="false">Contacts
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+                <div id="myTabContent">
+                    <div className="bg-gray-50 p-4 rounded-lg dark:bg-gray-800 hidden" id="profile" role="tabpanel"
+                         aria-labelledby="profile-tab">
+                        <p className="text-gray-500 dark:text-gray-400 text-sm">C’est la première mission de notre Cabinet : encadrer vos recrutements, et la rédaction ou la modification du contrat de travail selon la législation en vigueur. En outre, au regard des conséquences financières (et sur l’environnement de travail) qu’un litige peut engendrer, recourir à un avocat spécialisé en droit du travail pour préparer et/ou négocier la fin d’une relation de travail s’avère indispensable.
+
+                            Le Cabinet Babeau est le partenaire de nombreuses entreprises de toutes tailles. Nous vous accompagnons dans la résolution de toutes les problématiques liées au droit du travail (rupture conventionnelle, sanction disciplinaire, licenciement, modification du contrat de travail, congés payés, rémunération variable, mobilité…).</p>
+                    </div>
+                    <div className="bg-gray-50 p-4 rounded-lg dark:bg-gray-800" id="dashboard" role="tabpanel"
+                         aria-labelledby="dashboard-tab">
+                        <p className="text-gray-500 dark:text-gray-400 text-sm">This is some placeholder content
+                            the <strong className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated
+                                content</strong>. Clicking another tab will toggle the visibility of this one for the
+                            next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                    </div>
+                    <div className="bg-gray-50 p-4 rounded-lg dark:bg-gray-800 hidden" id="settings" role="tabpanel"
+                         aria-labelledby="settings-tab">
+                        <p className="text-gray-500 dark:text-gray-400 text-sm">This is some placeholder content
+                            the <strong className="font-medium text-gray-800 dark:text-white">Settings tab's associated
+                                content</strong>. Clicking another tab will toggle the visibility of this one for the
+                            next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                    </div>
+                    <div className="bg-gray-50 p-4 rounded-lg dark:bg-gray-800 hidden" id="contacts" role="tabpanel"
+                         aria-labelledby="contacts-tab">
+                        <p className="text-gray-500 dark:text-gray-400 text-sm">This is some placeholder content
+                            the <strong className="font-medium text-gray-800 dark:text-white">Contacts tab's associated
+                                content</strong>. Clicking another tab will toggle the visibility of this one for the
+                            next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <script src="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/flowbite.bundle.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div className={"w-3/4 mx-auto flex bg-white"}>
                 <div className={"flex justify-center mt-10"}>
                     <div className={"flex flex-col items-center gap-y-5"}>
                         <div className={"relative w-4/5 overflow-hidden"}>
