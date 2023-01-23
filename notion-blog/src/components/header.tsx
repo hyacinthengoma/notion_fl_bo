11f80c0cc14d11d334549cb4b9864fc44f2e5c03
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2023-01-23 17:06:09 +0100

    modif svg

--- notion-blog/src/components/header.tsx
+++ notion-blog/src/components/header.tsx
@@ -150,7 +150,7 @@ export default function Header() {
                             <svg className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500"
                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                  stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                       d="M6 18L18 6M6 6l12 12"></path>
                             </svg>
                         </button>
