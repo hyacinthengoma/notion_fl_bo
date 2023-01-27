bdf0e88abde1123efffb819885d065e729b2b23e
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2023-01-27 15:01:47 +0100

    LOADER POGGERS

--- notion-blog/src/pages/_app.tsx
+++ notion-blog/src/pages/_app.tsx
@@ -4,6 +4,8 @@ import 'katex/dist/katex.css'
 import Footer from '../components/footer'
 import { Poppins } from '@next/font/google';
 import NextNProgress from "nextjs-progressbar";
+import {useEffect} from "react";
+import Loader from "../components/loader";
 
 const poppins = Poppins({
     weight: ['400', '700'],
@@ -12,11 +14,19 @@ const poppins = Poppins({
 });
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-        <NextNProgress height={6} color="#4338C9"></NextNProgress>
-      <Component {...pageProps} />
-      <Footer />
-    </>
-  )
+    // Hide splash screen shen we are server side
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const loader = document.getElementById('globalLoader');
+            if (loader)
+                loader.style.display = 'none';
+        }
+    }, []);
+    return (
+        <>
+            <NextNProgress height={6} color="#4338C9"></NextNProgress>
+          <Component {...pageProps} />
+          <Footer />
+        </>
+      )
 }
--- notion-blog/src/pages/_document.tsx
+++ notion-blog/src/pages/_document.tsx
@@ -1,6 +1,32 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Loader from '../components/loader';
 
 class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head/>
+                <head>
+                    <style>
+                        {Loader}
+                    </style>
+                </head>
+                <body>
+                <div id={'globalLoader'}>
+                    <div className="loader">
+                        <div/>
+                        <div/>
+                    </div>
+                </div>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
+
+/**class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
@@ -12,6 +38,6 @@ class MyDocument extends Document {
       </Html>
     )
   }
-}
+}*/
 
 export default MyDocument
