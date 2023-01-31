f4bb2fb218e1198497366a101e969352f74d6d08
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2023-01-31 19:16:00 +0100

    commit

--- notion-blog/src/components/BackToTop.tsx
+++ notion-blog/src/components/BackToTop.tsx
@@ -23,6 +23,8 @@ export default function BackToTop() {
         function topFunction() {
             document.body.scrollTop = 0; // For Safari
             document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+            document.body.style.scrollBehavior = "smooth"
+            document.documentElement.style.scrollBehavior = "smooth"
         }
     });
     return (
--- notion-blog/src/pages/Expertise/Droit-securite-sociale/index.tsx
+++ notion-blog/src/pages/Expertise/Droit-securite-sociale/index.tsx
@@ -41,13 +41,12 @@ export default function HomeDroitSecuSociale() {
                                 <h2 className={"lg:text-xl text-base text-red-900 font-semibold text-white"}>Contrat de travail et relations individuelles</h2>
                             </div>
                             <div className={"absolute top-3 right-3 text-red-900 transition-transform duration-500 rotate-0 peer-checked:rotate-180"}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round"
-                                          d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
                                 </svg>
+
                             </div>
-                            <div className={"bg-white  overflow-hidden transition-all duration-500 max-h-0 peer-checked:max-h-full"}>
+                            <div className={"bg-white  overflow-hidden transition-all duration-500 max-h-full peer-checked:max-h-0"}>
                                 <div className={"p-4"}>
                                     <p>C’est la première mission de notre Cabinet : encadrer vos recrutements, et la rédaction ou la modification du contrat de travail selon la législation en vigueur. En outre, au regard des conséquences financières (et sur l’environnement de travail) qu’un litige peut engendrer, recourir à un avocat spécialisé en droit du travail pour préparer et/ou négocier la fin d’une relation de travail s’avère indispensable.
 
@@ -59,8 +58,8 @@ export default function HomeDroitSecuSociale() {
 
                         <div className={"relative w-4/5 overflow-hidden"}>
                             <input type={"checkbox"} className={"absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer peer"}/>
-                            <div className={"bg-white h-12 w-full pl-5 flex items-center"}>
-                                <h2 className={"lg:text-xl text-base text-red-900 font-semibold text-white peer-hover:underline"}>Relations collectives et restructurations</h2>
+                            <div className={"bg-white h-12 w-full pl-5 flex items-center peer-hover:underline"}>
+                                <h2 className={"lg:text-xl text-base text-red-900 font-semibold text-white"}>Relations collectives et restructurations</h2>
                             </div>
                             <div className={"absolute top-3 right-3 text-red-900 transition-transform duration-500 rotate-0 peer-checked:rotate-180"}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -81,8 +80,8 @@ export default function HomeDroitSecuSociale() {
 
                         <div className={"relative w-4/5 overflow-hidden"}>
                             <input type={"checkbox"} className={"absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer peer"}/>
-                            <div className={"bg-white h-12 w-full pl-5 flex items-center"}>
-                                <h2 className={"lg:text-xl text-base font-semibold text-red-900 peer-hover:underline"}>Contentieux du travail et du licenciement</h2>
+                            <div className={"bg-white h-12 w-full pl-5 flex items-center peer-hover:underline"}>
+                                <h2 className={"lg:text-xl text-base font-semibold text-red-900"}>Contentieux du travail et du licenciement</h2>
                             </div>
                             <div className={"absolute top-3 right-3 text-red-900 transition-transform duration-500 rotate-0 peer-checked:rotate-180"}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -103,8 +102,8 @@ export default function HomeDroitSecuSociale() {
 
                         <div className={"relative w-4/5 overflow-hidden"}>
                             <input type={"checkbox"} className={"absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer peer"}/>
-                            <div className={"bg-white h-12 w-full pl-5 flex items-center"}>
-                                <h2 className={"lg:text-xl text-base font-semibold text-red-900 peer-hover:underline"}>Droit et contentieux de la sécurité sociale</h2>
+                            <div className={"bg-white h-12 w-full pl-5 flex items-center peer-hover:underline"}>
+                                <h2 className={"lg:text-xl text-base font-semibold text-red-900"}>Droit et contentieux de la sécurité sociale</h2>
                             </div>
                             <div className={"absolute top-3 right-3 text-red-900 transition-transform duration-500 rotate-0 peer-checked:rotate-180"}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -125,8 +124,8 @@ export default function HomeDroitSecuSociale() {
 
                         <div className={"relative w-4/5 overflow-hidden"}>
                             <input type={"checkbox"} className={"absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer peer"}/>
-                            <div className={"bg-white h-12 w-full pl-5 flex items-center"}>
-                                <h2 className={"lg:text-xl text-base font-semibold text-red-900 peer-hover:underline"}>Droit pénal du travail</h2>
+                            <div className={"bg-white h-12 w-full pl-5 flex items-center peer-hover:underline"}>
+                                <h2 className={"lg:text-xl text-base font-semibold text-red-900"}>Droit pénal du travail</h2>
                             </div>
                             <div className={"absolute top-3 right-3 text-red-900 transition-transform duration-500 rotate-0 peer-checked:rotate-180"}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
