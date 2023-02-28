e27d0d602ac7942d201117f8b32f82997fff3e0e
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2023-02-28 11:12:09 +0100

    Update index.tsx

--- src/pages/Expertise/Droit-securite-sociale/index.tsx
+++ src/pages/Expertise/Droit-securite-sociale/index.tsx
@@ -3,7 +3,7 @@ import BesoinAvocat from "../../../components/Besoin-avocat";
 import getDroitSocialIndex from "../../../lib/notion/getDroitSocialIndex";
 import Head from "next/head";
 import React, {useEffect, useState} from "react";
-
+import { Collapse, Text, Grid, Avatar } from "@nextui-org/react";
 export async function getStaticProps({ preview }) {
     const droitSocialTable = await getDroitSocialIndex();
 
@@ -31,7 +31,7 @@ export default function HomeDroitSecuSociale({ droitSocial }) {
     const [detailsCompetenceSocial, setDetailsCompetencesSocial] = useState('');
     const [titreCompetenceSocial, setTitreCompetenceSocial] = useState('');
 
-    useEffect(() => {
+    {/**useEffect(() => {
         let overlay = document.querySelector("#overlay");
         let skills = document.querySelectorAll('.skill');
 
@@ -59,13 +59,13 @@ export default function HomeDroitSecuSociale({ droitSocial }) {
             }
         })
 
-    });
+    });*/}
     return (
         <div className={"relative"}>
             <Head>
                 <title>Expertise Droit de la sécurité sociale</title>
             </Head>
-            <div className={"left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 fixed bg-black/50 backdrop-blur-sm h-screen w-full z-[99] cursor-pointer transition-all duration-500 opacity-0 hidden"} id={"overlay"}>
+            {/**<div className={"left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 fixed bg-black/50 backdrop-blur-sm h-screen w-full z-[99] cursor-pointer transition-all duration-500 opacity-0 hidden"} id={"overlay"}>
                 <div className={"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full lg:w-auto"}>
                     <div className={"flex justify-center"}>
                         <div className="block p-10 bg-white h-auto w-4/5 rounded-xl border border-gray-100 p-4 focus:border-0 shadow transition-all duration-500 hover:ring-1 hover:ring-gray-300 focus:outline-none focus:ring cursor-default ">
@@ -82,7 +82,7 @@ export default function HomeDroitSecuSociale({ droitSocial }) {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>*/}
             <div className={"relative w-full"}>
                 <div className={"absolute transform top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-4/5 text-center z-50"}>
                     <h1 className={"text-white text-2xl md:text-4xl font-bold translate-y-1/2"}>DROIT DE LA SÉCURITÉ SOCIALE</h1>
@@ -93,54 +93,27 @@ export default function HomeDroitSecuSociale({ droitSocial }) {
                 <div className={"w-5/6"}>
                     <h2 className={"font-bold text-gray-800 text-4xl uppercase mb-6"}>Mes compténces</h2>
                     <hr/>
-                    <div className={"flex w-full justify-center mt-10"}>
-                        <div className={"flex flex-col items-center gap-y-10"}>
-                            {droitSocial.map((currentDroit, index) => {
-                                if(index === 0){
-                                    return(
-                                        <div className={"relative w-5/6 overflow-hidden"}>
-                                            <input type={"checkbox"} className={"absolute top-0 inset-x-0 w-full h-full opacity-0 z-10 cursor-pointer peer"}/>
-                                            <div className={"bg-white w-full pr-10 flex items-center peer-hover:underline"}>
-                                                <h2 className={"lg:text-xl text-base text-red-900 font-semibold text-red-900"}>{capitalizeFirstLetter(currentDroit.Slug.replace(/-/g, ' '))}</h2>
-                                            </div>
-                                            <div className={"absolute top-3 right-3 text-red-900 transition-transform duration-500 rotate-0 peer-checked:rotate-180"}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                        <Grid>
+                            <Collapse.Group shadow>
+                                {droitSocial.map((currentDroit, index) => {
+                                    return (
+                                        <Collapse title={<Text h4>{currentDroit.Titre}</Text>} contentLeft={
+                                            <span className="inline-block rounded-lg bg-red-100 p-3">
+                                                <svg className="h-6 w-6 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                                                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
                                                 </svg>
-
-                                            </div>
-                                            <div className={"bg-white  overflow-hidden transition-all duration-500 max-h-full peer-checked:max-h-0"}>
-                                                <div className={"pt-4"}>
-                                                    <p>{ currentDroit.Texte }</p>
-                                                </div>
-                                            </div>
-                                        </div>
+                                            </span>
+                                            }>
+                                            <Text>
+                                                {currentDroit.Texte}
+                                            </Text>
+                                        </Collapse>
                                     );
-                                }else{
-                                    return(
-                                        <div className={"relative w-5/6 overflow-hidden"}>
-                                            <input type={"checkbox"} className={"absolute top-0 inset-x-0 w-full h-full opacity-0 z-10 cursor-pointer peer"}/>
-                                            <div className={"bg-white w-full pr-10 flex items-center peer-hover:underline"}>
-                                                <h2 className={"lg:text-xl text-base font-semibold text-red-900"}>{capitalizeFirstLetter(currentDroit.Slug.replace(/-/g, ' '))}</h2>
-                                            </div>
-                                            <div className={"absolute top-3 right-3 text-red-900 transition-transform duration-500 rotate-0 peer-checked:rotate-180"}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                     strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                                    <path strokeLinecap="round" strokeLinejoin="round"
-                                                          d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
-                                                </svg>
-                                            </div>
-                                            <div className={"bg-white overflow-hidden transition-all duration-500 max-h-0 peer-checked:max-h-full"}>
-                                                <div className={"pt-4"}>
-                                                    <p>{ currentDroit.Texte }</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    );
-                                }
-                            })}
-                        </div>
-                    </div>
+                                })}
+                            </Collapse.Group>
+                        </Grid>
                     {/**<div className={"grid grid-cols-1 lg:grid-cols-4 gap-8 mt-10"}>
                         {droitSocial.map((currentDroit, index) => {
                             return(
