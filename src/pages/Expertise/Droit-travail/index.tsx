46d421beee9c9c745e6ff495c01f372ccca62dca
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2023-02-28 11:12:27 +0100

    Update index.tsx

--- src/pages/Expertise/Droit-travail/index.tsx
+++ src/pages/Expertise/Droit-travail/index.tsx
@@ -145,7 +145,7 @@ export default function HomeDroitTravail({ droitTravail }) {
                     <div className={"grid grid-cols-1 lg:grid-cols-4 gap-8 mt-10"}>
                         {droitTravail.map((currentDroit, index) => {
                             return(
-                                <div onClick={(e) => {setDetailsCompetences(currentDroit.Texte); setTitreCompentce(capitalizeFirstLetter(currentDroit.Slug.replace(/-/g, ' ')))}} className="block bg-white rounded-xl border border-gray-100 p-4 shadow hover:border-red-700 hover:shadow-red-50 hover:shadow-lg hover:scale-105 transition-all duration-500 hover:ring-1 hover:ring-gray-300 focus:outline-none focus:ring cursor-pointer skill">
+                                <div onClick={(e) => {setDetailsCompetences(currentDroit.Texte); setTitreCompentce(currentDroit.Titre)}} className="block bg-white rounded-xl border border-gray-100 p-4 shadow hover:border-red-700 hover:shadow-red-50 hover:shadow-lg hover:scale-105 transition-all duration-500 hover:ring-1 hover:ring-gray-300 focus:outline-none focus:ring cursor-pointer skill">
                                   <span className="inline-block rounded-lg bg-red-100 p-3">
                                     <svg className="h-6 w-6 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                       <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
@@ -153,7 +153,7 @@ export default function HomeDroitTravail({ droitTravail }) {
                                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
                                     </svg>
                                   </span>
-                                    <h2 className="mt-2 font-thin text-gray-800">{capitalizeFirstLetter(currentDroit.Slug.replace(/-/g, ' '))}</h2>
+                                    <h2 className="mt-2 font-thin text-gray-800">{currentDroit.Titre}</h2>
                                 </div>
                             );
                         })}
