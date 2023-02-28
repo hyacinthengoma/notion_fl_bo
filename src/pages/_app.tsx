5fa797f272f4208721e9761d862b0fecccb2acc4
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2023-02-28 11:11:00 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import NextNProgress from "nextjs-progressbar";
 import {useEffect} from "react";
 import Header from "../components/header";
 import BackToTop from "../components/BackToTop";
+import { NextUIProvider } from '@nextui-org/react';
 
 const poppins = Poppins({
     weight: ['400', '700'],
@@ -16,42 +17,28 @@ const poppins = Poppins({
 export default function MyApp({ Component, pageProps }) {
     // Hide splash screen shen we are server side
     useEffect(() => {
-        const burger = document.querySelectorAll('.navbar-burger');
-        const menu = document.querySelectorAll('.navbar-menu');
 
-        if (burger.length && menu.length) {
-            for (var i = 0; i < burger.length; i++) {
-                burger[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
+        const burger = document.querySelector('#navbar-burger');
+        const menu = document.querySelector('#navbar-menu');
+        const close = document.querySelector('#navbar-close');
+        const backdrop = document.querySelector('#navbar-backdrop');
+        const sidemenuLinks = document.querySelectorAll('#sidemenu a');
 
-        //TODO : Revoir les dropdown expertise
-        const close = document.querySelectorAll('.navbar-close');
-        const backdrop = document.querySelectorAll('.navbar-backdrop');
+        burger.addEventListener('click', function(){
+            menu.classList.toggle('hidden');
+        });
+        close.addEventListener('click', function(){
+            menu.classList.toggle('hidden');
+        });
+        backdrop.addEventListener('click', function(){
+            menu.classList.toggle('hidden');
+        });
 
-        if (close.length) {
-            for (var i = 0; i < close.length; i++) {
-                close[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
-
-        if (backdrop.length) {
-            for (var i = 0; i < backdrop.length; i++) {
-                backdrop[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
+        Array.prototype.forEach.call(sidemenuLinks, function(currentSideMenuLink) {
+            currentSideMenuLink.addEventListener('click', function(){
+                menu.classList.toggle('hidden');
+            })
+        });
 
         if (typeof window !== 'undefined') {
             const loader = document.getElementById('globalLoader');
@@ -69,11 +56,15 @@ export default function MyApp({ Component, pageProps }) {
     }, []);
     return (
         <>
-            <NextNProgress height={6} color="#4338C9"></NextNProgress>
-            <Header></Header>
-            <Component {...pageProps} />
-            <BackToTop></BackToTop>
-            <Footer />
+
+                <NextNProgress height={6} color="#4338C9"></NextNProgress>
+                <Header></Header>
+                <NextUIProvider>
+                    <Component {...pageProps} />
+                </NextUIProvider>
+                <BackToTop></BackToTop>
+                <Footer />
+
         </>
       )
 }
