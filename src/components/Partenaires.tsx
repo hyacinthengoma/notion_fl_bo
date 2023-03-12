898de0b373af9ba598c8361baf014487b1387809
Hyacinthe N'Goma <hyacinthengoma@gmail.com>
2023-03-12 15:45:52 +0100

    add devider partner block

--- src/components/Partenaires.tsx
+++ src/components/Partenaires.tsx
@@ -5,9 +5,10 @@ import Image from "next/image";
 
 export default function Partenaires(){
     return(
-        <div className={"bg-white flex justify-center w-full py-20 pl-4 pr-5"}>
+        <div className={"bg-white flex justify-center pb-16 w-full pl-4 pr-5"}>
             <div className={"w-5/6"}>
-                <h2 className={"font-bold text-black text-xl lg:text-3xl uppercase mb-6"}>MES PARTENAIRES</h2>
+        <hr />
+                <h2 className={"font-bold text-black text-xl lg:text-3xl uppercase mb-6 py-16"}>MES PARTENAIRES</h2>
                 <div className={"items-center sm:justify-between flex flex-col lg:flex-row w-full gap-10"}>
                     <Image src={"/images/Partenaires/carsat-aquitaine.png"} className={"w-40"} alt={"Logo carsat aquitaine"} width={1000} height={1000}/>
                     <Image src={"/images/Partenaires/extencia.png"} className={"w-40"} alt={"Logo extencia"} width={500} height={500}/>
