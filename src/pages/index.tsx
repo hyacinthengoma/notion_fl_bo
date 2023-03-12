04af508c388475af08aca8c5d2fe098d1a234894
Hyacinthe N'Goma <hyacinthengoma@gmail.com>
2023-03-12 17:55:07 +0100

    add cursor hover on home video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -182,7 +182,7 @@ const Index = ({ posts = [], accueil, competencesSocial, competencesTravail}) =>
                                 </div>
                                 <div className={"my-auto"}>
                                     <div className="h-auto md:h-auto overflow-hidden rounded-lg shadow-lg">
-                                        <video controls poster="/images/FlorenceBabeau/poster.png" src={"/video/FlorenceBabeau.mp4"} type={"video/mp4"} className={""}></video>
+                                        <video controls poster="/images/FlorenceBabeau/poster.png" src={"/video/FlorenceBabeau.mp4"} type={"video/mp4"} className={"hover:cursor-pointer"}></video>
                                     </div>
                                 </div>
                             </div>
