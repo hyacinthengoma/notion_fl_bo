cee0c466022f6b8a6ddbd8ba1aa94f54870c035e
Alexandre P <34238909+Deltouille@users.noreply.github.com>
2023-03-20 22:20:01 +0100

    commit

--- src/pages/Expertise/Droit-securite-sociale/index.tsx
+++ src/pages/Expertise/Droit-securite-sociale/index.tsx
@@ -94,47 +94,32 @@ export default function HomeDroitSecuSociale({ droitSocial }) {
                 <div className={"w-5/6"}>
                     <h2 className={"font-bold text-gray-800 text-xl lg:text-3xl uppercase mb-6"}>Nos compétences</h2>
                     <hr className={"mb-5"}/>
-                        <div className={"lg:w-2/3 lg:mx-auto"}>
-                            <Grid>
-                                <Collapse.Group shadow>
-                                    {droitSocial.map((currentDroit, index) => {
-                                        if(index === 0) {
-                                            return (
-                                                <Collapse title={<Text h4>{currentDroit.Titre}</Text>} contentLeft={
-                                                    <span className="inline-block rounded-lg bg-red-100 p-3">
+                    <div className="grid gap-4 sm:grid-cols-2 md:gap-8">
+                        {droitSocial.map((currentDroit, index) => {
+                            let tableauDeSousChaines = currentDroit.Texte.split("\\n");
+                            let chaineAvecSautDeLigne = tableauDeSousChaines.join("\n");
+                            return(
+                                <div className="shadow bg-white p-5 hover:border-red-800 transition-all duration-500 border">
+                                    <div className="mb-4 flex items-center justify-between gap-4 border-b pb-4">
+                                        <h3 className="font-semibold text-gray-800 sm:text-lg md:text-xl">{currentDroit.Titre}</h3>
+                                        <span className="inline-block rounded-lg bg-red-100 p-3">
                                                 <svg className="h-6 w-6 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                   <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
                                                   <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
                                                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
                                                 </svg>
                                             </span>
-                                                } expanded={true}>
-                                                    <Text>
-                                                        {currentDroit.Texte}
-                                                    </Text>
-                                                </Collapse>
-                                            );
-                                        }else{
-                                            return (
-                                                <Collapse title={<Text h4>{currentDroit.Titre}</Text>} contentLeft={
-                                                    <span className="inline-block rounded-lg bg-red-100 p-3">
-                                                <svg className="h-6 w-6 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                                                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
-                                                </svg>
-                                            </span>
-                                                }>
-                                                    <Text>
-                                                        {currentDroit.Texte}
-                                                    </Text>
-                                                </Collapse>
-                                            );
-                                        }
-                                    })}
-                                </Collapse.Group>
-                            </Grid>
-                        </div>
+                                    </div>
+
+                                    <p className="text-gray-500">
+                                        {tableauDeSousChaines.map((line, index) => (
+                                            <p key={index} className={"mt-2 text-justify"}>{line}</p>
+                                        ))}
+                                    </p>
+                                </div>
+                            );
+                        })}
+                    </div>
                     {/**<div className={"grid grid-cols-1 lg:grid-cols-4 gap-8 mt-10"}>
                         {droitSocial.map((currentDroit, index) => {
                             return(
